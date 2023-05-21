0a6fa733a725f8eec9469d7f67c7a7fb3320eef8
chris <chris.dev.edu@gmail.com>
2023-05-21 13:28:37 +0900

    lecture 02

---
+++ lecture02/problem/p08.py
@@ -0,0 +1,38 @@
+"""
+1. students에는 5명의 학생 정보를 담은 dictionary들이 list로 담겨있다.
+2. 1년이 지나서, 모든 학생들의 나이를 1살씩 올려주고, grade가 3인 학생은 졸업을 하여 list에서 제거해준다.
+3. 2번처럼 데이터를 업데이트 후, score가 A인 학생들의 이름을 출력한다.
+"""
+
+students = [
+    {
+        'name': 'Amy',
+        'age': 15,
+        'grade': 2,
+        'score': 'A',
+    },
+    {
+        'name': 'Bob',
+        'age': 16,
+        'grade': 3,
+        'score': 'C',
+    },
+    {
+        'name': 'Chris',
+        'age': 14,
+        'grade': 1,
+        'score': 'C',
+    },
+    {
+        'name': 'Jenny',
+        'age': 14,
+        'grade': 1,
+        'score': 'A',
+    },
+    {
+        'name': 'Paul',
+        'age': 16,
+        'grade': 3,
+        'score': 'B',
+    },
+]
